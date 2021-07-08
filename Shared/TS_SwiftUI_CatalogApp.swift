ed2043b73c0f15a9f39e986d2a5c7b8123250398
Thomas Sillmann <contact@thomassillmann.de>
2021-07-08 09:08:35 +0200

    Initial Commit

---
+++ Shared/TS_SwiftUI_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TS_SwiftUI_CatalogApp.swift
+//  Shared
+//
+//  Created by Thomas Sillmann on 08.07.21.
+//
+
+import SwiftUI
+
+@main
+struct TS_SwiftUI_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
